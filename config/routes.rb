054dfa4b9aaf6a00774001a021e2795bebd003c4
nonamme <lewandowskikamil@yahoo.co.uk>
2016-01-08 09:42:42 +0000

    Adding delete action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  
-  resources :posts do
-  	resources :comments
-  end
+    
+  devise_for :users
+  	resources :posts do
+		resources :comments
+	end
 
-  root "posts#index"
-
-  get '/about', to: 'pages#about'
+  	root to: "posts#index"
+  	
+  	get '/about', to: 'pages#about'
 end
